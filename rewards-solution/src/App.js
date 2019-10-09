472074eb62f72cd9fc8c2699878dea18649ffbab
izuua <izuuaaf@gmail.com>
2019-10-09 16:08:51 -0400

    Added the wrapper component.

--- rewards-solution/src/App.js
+++ rewards-solution/src/App.js
@@ -3,6 +3,10 @@ import transactions from "./models/transactions.json"
 import './App.css';
 
 class App extends React.Component {
+  state = {
+    transactions
+  }
+
   render() {
     return (
       <h1>Hello World!</h1>
